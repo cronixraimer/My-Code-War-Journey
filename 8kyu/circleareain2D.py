64b57b72930d261cd3c0cac28679b3c6ce002923
Cronix Raimer <ayubovbakhtiyor@gmail.com>
2023-12-29 10:22:14 +0400

    basic geometry circle area 2D

---
+++ 8kyu/circleareain2D.py
@@ -0,0 +1,20 @@
+import math
+
+class Circle:
+    def __init__ (center, radius):
+        this.center = cente;
+        this.radius = radius
+
+class Point:
+    def __init__ (x,y):
+        this.x = x
+        this.y = y
+
+
+def circle_are(circle):
+    return round(math.pi * circle.radius**2, 6)
+
+center = Point(0, 0)
+my_cicrle = Circle(center, 5)
+area = circle_are(my_cicrle)
+print(area)
