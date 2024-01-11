03363e36203de15cea16ff30866d487c8a74f06e
Cronix Raimer <ayubovbakhtiyor@gmail.com>
2024-01-11 11:29:56 +0400

    gcd math simple one

---
+++ 7kyu/greatestcommendivisor.py
@@ -0,0 +1,8 @@
+import math
+def mygcd(x, y):
+    return math.gcd(x, y)
+
+result = mygcd(30, 12)
+print(result)
+
+#this one was way too easy if you how to use library also best solution was same in the website 
